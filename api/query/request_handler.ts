4eb5115f9d570c58b506e059659f0ffa6a868e91
professorxcell25@gmail.com <professorxcell25@example.com>
2026-06-15 15:06:03 -0400

    Adding build context to request handler

--- api/query/request_handler.ts
+++ api/query/request_handler.ts
@@ -1,10 +1,13 @@
 import { createYoga } from 'graphql-yoga'
 import type { FastifyRequest, FastifyReply } from 'fastify'
 import { graphql_schema } from '../database/schema.js'
+import { build_context } from '../database/context.js'
+
 export const yoga = createYoga<{
     req: FastifyRequest
     reply: FastifyReply
 }>({
     schema: graphql_schema,
-    graphqlEndpoint: '/graphql'
+    graphqlEndpoint: '/graphql',
+    context: build_context
 })
