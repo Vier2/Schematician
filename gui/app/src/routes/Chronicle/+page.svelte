cb2649840684a0ca59ab64e8635ffe1ea446e7a5
professorxcell25@gmail.com <professorxcell25@example.com>
2026-06-16 11:35:54 -0400

    Made Schema definition button

--- gui/app/src/routes/Chronicle/+page.svelte
+++ gui/app/src/routes/Chronicle/+page.svelte
@@ -1,12 +1,25 @@
 <div class="main">
     <div class="header" id="header"> 
-        <button> New Schema</button>
+        <button id="new_schema_button"> New Schema</button>
         <div id="facted_search"> Faceted Search </div>
         <button> Settings </button>
     </div>
     <div id="Chronicle"> Chronicle</div>
 
 </div>
+<script lang="ts">
+import { onMount } from "svelte";
+import { browser } from "$app/environment";
+import { Make_Button_Goto_URL } from "$lib/utils";
+    onMount(() => {
+      if (browser) {
+            const new_schema_button: HTMLButtonElement = document.getElementById('new_schema_button') as HTMLButtonElement
+
+            Make_Button_Goto_URL(new_schema_button, 'Schema/Definition')
+        }
+            });
+</script>
+
 <style>
     .main {
         display: grid;
