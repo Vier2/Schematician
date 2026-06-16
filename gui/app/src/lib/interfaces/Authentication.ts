aaf9cecb494a2ab8ea50ba0c531249be2549c11a
professorxcell25@gmail.com <professorxcell25@example.com>
2026-06-16 09:47:24 -0400

    Login and Register page

--- gui/app/src/lib/interfaces/Authentication.ts
+++ gui/app/src/lib/interfaces/Authentication.ts
@@ -2,43 +2,37 @@ export interface login {
     email: string
     password: string
 }
+
 export interface register {
     username: string
     email: string
     password: string
 }
-// Option 1: Type alias for function
+
 export type Authentication_Method = (credentials: FormData) => Object
 
- 
 export interface LoginResponse {
     data: {
         login: {
-            token: string;
-            user: {
-                username: string;
-                email: string;
-            }
+            token: string
+            user_uid: string
         }
-    };
+    }
     errors?: Array<{
-        message: string;
-        locations?: Array<{ line: number; column: number }>;
-    }>;
+        message: string
+        locations?: Array<{ line: number; column: number }>
+    }>
 }
 
 export interface RegisterResponse {
     data: {
         register: {
-            token: string;
-            user: {
-                username: string;
-                email: string
-            }
+            token: string
+            user_uid: string
         }
-    };
+    }
     errors?: Array<{
-        message: string;
-        locations?: Array<{ line: number; column: number }>;
-    }>;
+        message: string
+        locations?: Array<{ line: number; column: number }>
+    }>
 }
---
+++ gui/app/src/routes/Home/Oracle/+page.svelte
@@ -0,0 +1,3 @@
+<div>
+    Hello Welcome User
+</div>
