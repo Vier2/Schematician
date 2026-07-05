b0272bd31db029a03eee3f71806c2216549d606d
professorxcell25@gmail.com <professorxcell25@example.com>
2026-07-05 14:04:28 -0400

    created mutation

---
+++ api/database/entities/instances/mutation.ts
@@ -0,0 +1,43 @@
+import { builder } from "../../builder.js";
+import type { GraphQL_Instance } from "./schema.js";
+import { db_create_instance } from "./repository.js";
+import { v4 as uuidv4 } from 'uuid'
+
+
+export const Instance_Ref = builder.objectRef<GraphQL_Instance>('Instance')
+
+
+const Instance_Value_Input = builder.inputType('Instance_Value_Input', {
+    fields: t => ({
+        field_schema_uid: t.string({ required: true }),
+        value: t.field({
+            type: 'JSON',
+            required: false
+        })
+    })
+})
+builder.mutationFields(t => ({
+    create_instance: t.field({
+        type: Instance_Ref,
+        args: {
+            schema_uid: t.arg.string({ required: true }),
+            objects: t.arg({
+                type: [Instance_Value_Input],
+                required: true
+            })
+        },
+        resolve: (_root, args, context) => {
+            if (!context.user) throw new Error('Unauthorized')
+
+            return db_create_instance(
+                context.driver,
+                context.user.id,
+                {
+                    uid: uuidv4(),
+                    schema_uid: args.schema_uid,
+                    objects: args.objects as any
+                }
+            )
+        }
+    })
+}))
